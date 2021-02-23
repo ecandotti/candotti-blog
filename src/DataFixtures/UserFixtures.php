ad33111d7c7e9b52e26176c23cb3e404fb2dd853
ecandotti <enzo.candotti16@hotmail.com>
2021-02-23 04:35:19 +0100

    fixture which create user and admin account

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /** @var UserPasswordEncoderInterface */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user
+            ->setAuthor('simple-user')
+            ->setFirstName('Barry')
+            ->setLastName('Alen')
+            ->setPhone('+33 123456789')
+            ->setEmail('user@candotti-blog.com')
+            ->setPassword($this->encoder->encodePassword($user, 'password'))
+            ->setRoles(['ROLE_USER'])
+        ;
+        
+        $manager->persist($user);
+        $this->addReference('simple-user', $user);
+
+        $admin = new User();
+        $admin
+            ->setAuthor('admin')
+            ->setFirstName('Suprem')
+            ->setLastName('Admin')
+            ->setPhone('+33 66666666')
+            ->setEmail('admin@candotti-blog.com')
+            ->setPassword($this->encoder->encodePassword($user, 'password'))
+            ->setRoles(['ROLE_ADMIN'])
+        ;
+        
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
