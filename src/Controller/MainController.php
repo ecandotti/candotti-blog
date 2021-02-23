9a53b363e6f6ea5daac66e2283a9c712d034c8f6
ecandotti <enzo.candotti16@hotmail.com>
2021-02-23 04:33:58 +0100

    route : Home, Contact, About, Newsletter form

---
+++ src/Controller/MainController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\NewsLetter;
+use App\Form\NewsLetterType;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function home(Request $request, PaginatorInterface $paginator): Response
+    {
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findBy([]);
+
+        $articles = $paginator->paginate(
+            $articles, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            10 // Nombre de résultats par page
+        );
+
+        return $this->render('base.html.twig', compact('articles'));
+    }
+
+    /**
+     * @Route("/", name="newsletter", methods={"POST"})
+     */
+    public function newsletter(Request $request)
+    {
+        $newsletter = new NewsLetter();
+        $form = $this->createForm(NewsLetterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Inscription à la newsletter réussis !');
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+}
