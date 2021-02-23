d9091e387f4ca961be27e50ecd692e77f2fa0cdd
ecandotti <enzo.candotti16@hotmail.com>
2021-02-23 04:36:53 +0100

    register form for register... :(

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'Email',
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes doivent correspondre.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer votre mot de passe'],
+            ])
+            ->add('author', TextType::class, [
+                'required' => true,
+                'label' => 'Nom d\'autheur',
+            ])
+            ->add('first_name', TextType::class, [
+                'required' => true,
+                'label' => 'Prénom',
+            ])
+            ->add('last_name', TextType::class, [
+                'required' => true,
+                'label' => 'Nom de famille',
+            ])
+            ->add('phone', TelType::class, [
+                'required' => true,
+                'label' => 'Numéro de téléphone',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
