bdbc50f741cfd8e8f5376d3fae22a8ee09756f3d
ecandotti <enzo.candotti16@hotmail.com>
2021-02-23 04:36:19 +0100

    Article Form for Edit/New/Update article

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label' => 'Titre',
+            ])
+            ->add('subtitle', TextType::class, [
+                'required' => true,
+                'label' => 'Sous Titre',
+            ])
+            ->add('readTime', NumberType::class, [
+                'required' => true,
+                'label' => 'Temps de lecture',
+            ])
+            ->add('content', TextareaType::class, [
+                'required' => true,
+                'label' => 'Contenu',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
