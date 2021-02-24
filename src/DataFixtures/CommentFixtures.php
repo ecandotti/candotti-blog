e78d13f6c1d9bb9c5e3621ed6f62453006ba8139
ecandotti <enzo.candotti16@hotmail.com>
2021-02-24 16:46:36 +0100

    fixtures to see fake comment

---
+++ src/DataFixtures/CommentFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Comment;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class CommentFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i=1; $i < 31; $i++) { 
+            for ($j=1; $j < 5; $j++) {
+                $comment = new Comment();
+                $comment
+                    ->setUser($this->getReference('simple-user'))
+                    ->setArticle($this->getReference('article'.$i))
+                    ->setActif(true)
+                    ->setContent('foiqjfpozeijrozjerfj => '.$j)
+                    ->setCreateAt(new DateTime())
+                ;
+
+            }
+            $manager->persist($comment);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            ArticleFixtures::class,
+            UserFixtures::class,
+        );
+    }
+}
