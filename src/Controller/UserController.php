02df0497a3e7f39cfab9f61a624290040d20a6b6
ecandotti <enzo.candotti16@hotmail.com>
2021-03-01 06:26:43 +0100

    routes for different page of user's dashboard

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,10 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Entity\Like;
+use App\Entity\Share;
+use App\Entity\User;
+use App\Form\ChangePwdType;
+use App\Form\UserType;
+use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Require ROLE_USER for *every* controller method in this class.
@@ -21,62 +32,143 @@ class UserController extends AbstractController
      */
     public function userDashboard(): Response
     {
-        $latest_article = $this->getDoctrine()->getRepository(Article::class)->findBy([
+        $fifth_article_like = $this->getDoctrine()->getRepository(Like::class)->findBy([
             'user' => $this->getUser()
         ], [
             'createAt' => 'DESC',
         ],5);
 
-        $comment = $this->getDoctrine()->getRepository(Comment::class)->findBy([
-            'status' => 'W'
-        ]);
+        $fifth_article_share = $this->getDoctrine()->getRepository(Share::class)->findBy([
+            'user' => $this->getUser()
+        ], [
+            'createAt' => 'DESC',
+        ],5);
 
-        if($comment) {
-            $this->addFlash('msg', 'Des commentaires sont à gérer.');
-        }
+        $fifth_article_comment = $this->getDoctrine()->getRepository(Comment::class)->findBy([
+            'user' => $this->getUser()
+        ], [
+            'createAt' => 'DESC',
+        ],5);
 
         return $this->render('user/dashboard.html.twig', [
-            'latest_article' => $latest_article,
+            'fifth_article_like' => $fifth_article_like,
+            'fifth_article_share' => $fifth_article_share,
+            'fifth_article_comment' => $fifth_article_comment,
         ]);
     }
 
     /**
      * @Route("/info", name="user_info")
      */
-    public function userInfo(): Response
+    public function userInfo(Request $request): Response
     {
-        return $this->render('user/info.html.twig');
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'Information édité avec succès');
+            return $this->redirectToRoute('user_info');
+        }
+
+        return $this->render('user/info.html.twig', [
+            'userForm' => $form->createView(),
+        ]);
     }
 
     /**
      * @Route("/password", name="user_password")
      */
-    public function userPassword(): Response
+    public function userPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder, UserRepository $userRepo): Response
     {
-        return $this->render('user/password.html.twig');
+        $form = $this->createForm(ChangePwdType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            
+            $formInfo = $form->getData();
+            $old_pwd = $formInfo['oldPassword']; 
+            $new_pwd = $formInfo['newPassword'];
+
+            $checkPass = $passwordEncoder->isPasswordValid($user, $old_pwd);
+            
+            if($checkPass) {
+                $userRepo->upgradePassword($user, $passwordEncoder->encodePassword($user, $new_pwd));
+            } else {
+                $this->addFlash('error', 'Mot de passe actuelle est erroné !');
+                return $this->redirectToRoute('user_password');
+            }
+
+            $this->addFlash('success', 'Mot de passe changé avec succès');
+            return $this->redirectToRoute('user_password');
+        }
+
+        return $this->render('user/password.html.twig', [
+            'passForm' => $form->createView(),
+        ]);
     }
 
     /**
      * @Route("/like", name="user_like")
      */
-    public function userLike(): Response
+    public function userLike(Request $request, PaginatorInterface $paginator): Response
     {
-        return $this->render('user/like.html.twig');
+        $articles_like = $this->getDoctrine()->getRepository(Like::class)->findBy([
+            'user' => $this->getUser()
+        ],['createAt' => 'DESC']);
+
+        $articles = $paginator->paginate(
+            $articles_like, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            10 // Nombre de résultats par page
+        );
+
+        return $this->render('user/like.html.twig', [
+            'articles' => $articles
+        ]);
     }
 
     /**
      * @Route("/share", name="user_share")
      */
-    public function userShare(): Response
+    public function userShare(Request $request, PaginatorInterface $paginator): Response
     {
-        return $this->render('user/share.html.twig');
+        $articles_share = $this->getDoctrine()->getRepository(Share::class)->findBy([
+            'user' => $this->getUser()
+        ],['createAt' => 'DESC']);
+
+        $articles = $paginator->paginate(
+            $articles_share, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            10 // Nombre de résultats par page
+        );
+
+        return $this->render('user/share.html.twig', [
+            'articles' => $articles
+        ]);
     }
 
     /**
      * @Route("/comment", name="user_comment")
      */
-    public function adminDashboard(): Response
+    public function adminDashboard(Request $request, PaginatorInterface $paginator): Response
     {
-        return $this->render('user/comment.html.twig');
+        $articles_comment = $this->getDoctrine()->getRepository(Comment::class)->findBy([
+            'user' => $this->getUser()
+        ],['createAt' => 'DESC']);
+
+        $articles = $paginator->paginate(
+            $articles_comment, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            10 // Nombre de résultats par page
+        );
+
+        return $this->render('user/comment.html.twig', [
+            'articles' => $articles
+        ]);
     }
 }
