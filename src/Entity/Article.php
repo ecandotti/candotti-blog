72631abce31528934f373029078d8d59ddb6fe9d
ecandotti <enzo.candotti16@hotmail.com>
2021-04-18 18:10:37 +0200

    add index for search feature

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ * @ORM\Table(name="article", indexes={@ORM\Index(columns={"title", "content"}, flags={"fulltext"})})
  */
 class Article
 {
